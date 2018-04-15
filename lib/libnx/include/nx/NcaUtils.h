ee04bcdc8d44cd946cba1f9c1fa57eb0c9ec207a
jakcron <jkrcarr@gmail.com>
2018-04-15 10:40:22 +0800

    [nx] Add NcaUtils

---
+++ lib/libnx/include/nx/NcaUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <nx/nca.h>
+
+namespace nx
+{
+	class NcaUtils
+	{
+	public:
+		static inline size_t sectorToOffset(size_t sector_index) { return sector_index * nx::nca::kSectorSize; }
+		static void decryptNcaHeader(const byte_t* src, byte_t* dst, const crypto::aes::sAesXts128Key& key);
+		static byte_t getMasterKeyRevisionFromKeyGeneration(byte_t key_generation);
+	};
+}
