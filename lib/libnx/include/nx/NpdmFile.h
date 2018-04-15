cdd11d58c666b6f94e0ba61438b33cf031e89ff1
jakcron <jkrcarr@gmail.com>
2018-04-15 14:01:41 +0800

    [nx] Removed unused file.

--- lib/libnx/include/nx/NpdmFile.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// structure
-// meta header
-// alignment
-// acid (aci desc sourced from base64 string)
-// -> acid is prefaced by a signature then rsa key (both 2048 bit)
-// alignment
-// aci0 (aci generated in realtime)
