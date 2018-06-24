4be0b51ddbc12893eabc6235c3ec446f79a649d4
jakcron <jkrcarr@gmail.com>
2018-06-24 17:17:21 +0800

    [fnd] Removed unused functions.

--- lib/libfnd/source/io.cpp
+++ lib/libfnd/source/io.cpp
@@ -11,9 +11,6 @@
 
 using namespace fnd;
 
-static const std::string kModuleName = "IO";
-static const size_t kBlockSize = 0x100000;
-
 size_t io::getFileSize(const std::string& path)
 {
 	std::ifstream f;
@@ -23,112 +20,6 @@ size_t io::getFileSize(const std::string& path)
 	std::ifstream::pos_type begin_pos = f.tellg();
 	f.seekg(0, std::ios_base::end);
 	return static_cast<size_t>(f.tellg() - begin_pos);
-}
-
-void io::readFile(const std::string& path, MemoryBlob & blob)
-{
-	fnd::SimpleFile file;
-	try
-	{
-		file.open(path, file.Read);
-	}
-	catch (const fnd::Exception&)
-	{
-		throw fnd::Exception(kModuleName, "Failed to open \"" + path + "\": does not exist");
-	}
-
-	size_t filesz, filepos;
-	filesz = file.size();
-
-
-	try {
-		blob.alloc(filesz);
-	}
-	catch (const fnd::Exception& e)
-	{
-		throw fnd::Exception(kModuleName, "Failed to allocate memory for file: " + std::string(e.what()));
-	}
-
-	for (filepos = 0; filesz > kBlockSize; filesz -= kBlockSize, filepos += kBlockSize)
-	{
-		file.read(blob.getBytes() + filepos, kBlockSize);
-	}
-
-	if (filesz)
-	{
-		file.read(blob.getBytes() + filepos, filesz);
-	}
-}
-
-void fnd::io::readFile(const std::string& path, size_t offset, size_t len, MemoryBlob& blob)
-{
-	fnd::SimpleFile file;
-	try
-	{
-		file.open(path, file.Read);
-	} catch (const fnd::Exception&)
-	{
-		throw fnd::Exception(kModuleName, "Failed to open \"" + path + "\": does not exist");
-	}
-
-	size_t filesz, filepos;
-	filesz = file.size();
-	file.seek(offset);
-
-	if (filesz < len || filesz < offset || filesz < (offset + len))
-	{
-		throw Exception(kModuleName, "Failed to open \"" + path + "\": file to small");
-	}
-
-	try
-	{
-		blob.alloc(filesz);
-	} catch (const fnd::Exception& e)
-	{
-		throw fnd::Exception(kModuleName, "Failed to allocate memory for file: " + std::string(e.what()));
-	}
-
-	for (filepos = 0; filesz > kBlockSize; filesz -= kBlockSize, filepos += kBlockSize)
-	{
-		file.read(blob.getBytes() + filepos, kBlockSize);
-	}
-
-	if (filesz)
-	{
-		file.read(blob.getBytes() + filepos, filesz);
-	}
-}
-
-void io::writeFile(const std::string& path, const MemoryBlob & blob)
-{
-	writeFile(path, blob.getBytes(), blob.getSize());
-}
-
-void io::writeFile(const std::string & path, const byte_t * data, size_t len)
-{
-	fnd::SimpleFile file;
-	try
-	{
-		file.open(path, file.Create);
-	} catch (const fnd::Exception&)
-	{
-		throw fnd::Exception(kModuleName, "Failed to open \"" + path + "\": does not exist");
-	}
-
-	size_t filesz, filepos;
-
-	filesz = len;
-
-
-	for (filepos = 0; filesz > kBlockSize; filesz -= kBlockSize, filepos += kBlockSize)
-	{
-		file.write(data + filepos, kBlockSize);
-	}
-
-	if (filesz)
-	{
-		file.write(data + filepos, filesz);
-	}
 }
 
 void io::makeDirectory(const std::string& path)
