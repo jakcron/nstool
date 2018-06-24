91c2d401113a148d474794fd79342f0a427f9145
jakcron <jkrcarr@gmail.com>
2018-06-24 16:18:29 +0800

    [fnd] Begin to deprecate some macros.

--- lib/libfnd/include/fnd/types.h
+++ lib/libfnd/include/fnd/types.h
@@ -6,8 +6,11 @@
 
 typedef uint8_t byte_t;
 
-#define MIN(x,y) ((x) <= (y)? (x) : (y))
-#define MAX(x,y) ((x) >= (y)? (x) : (y))
+#define _MIN(x,y) ((x) <= (y)? (x) : (y))
+#define _MAX(x,y) ((x) >= (y)? (x) : (y))
+
+#define MIN(x,y) (_MIN(x,y))
+#define MAX(x,y) (_MAX(x,y))
 
 static inline uint64_t align(uint64_t size, uint64_t align)
 {
