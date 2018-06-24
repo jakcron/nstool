9b3a26806a8af4d4a5ea685f156ce08fbc19de21
jakcron <jkrcarr@gmail.com>
2018-06-24 12:45:45 +0800

    [fnd] Added Vec, changed List. Remove MemoryBlob, replaced with Vec<byte_t>.

---
+++ lib/libfnd/include/fnd/ISerialisable.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <fnd/types.h>
+#include <fnd/Vec.h>
+
+namespace fnd
+{
+	class ISerialisable
+	{
+	public:
+		// serialise
+		virtual void toBytes() = 0;
+		// deserialise
+		virtual void fromBytes(const byte_t* data, size_t len) = 0;
+
+		// get byte vector
+		virtual const fnd::Vec<byte_t>& getBytes() const = 0;
+
+		// clear data
+		virtual void clear() = 0;
+	};
+}
--- lib/libfnd/include/fnd/List.h
+++ lib/libfnd/include/fnd/List.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <fnd/types.h>
-#include <vector>
+#include <fnd/Vec.h>
 
 namespace fnd
 {
@@ -8,130 +8,199 @@ namespace fnd
 	class List
 	{
 	public:
-		List() :
-			mElements()
-		{
-		}
+		// constructors
+		List();
+		List(const List<T>& other);
 
-		List(const T* elements, size_t num) :
-			mElements(num)
-		{
-			initList(elements, num);
-		}
+		// copy operator
+		const List<T>& operator=(const List<T>& other);
 
-		// assignment operator
-		const List& operator=(const List& other)
-		{
-			mElements.clear();
-			for (size_t i = 0; i < other.getSize(); i++)
-			{
-				mElements.push_back(other[i]);
-			}
+		// equivalence operators
+		bool operator==(const List<T>& other) const;
+		bool operator!=(const List<T>& other) const;
 
-			return *this;
-		}
+		// back relative insertion
+		void addElement(const T& element);
+		
+		// element access
+		const T& operator[](size_t index) const;
+		T& operator[](size_t index);
+		const T& atBack() const;
+		T& atBack();
 
-		// comparision operator
-		bool operator==(const List& other) const
-		{
-			if (other.getSize() != this->getSize())
-			{
-				return false;
-			}
+		// element num
+		size_t size() const;
 
-			for (size_t i = 0; i < this->getSize(); i++)
-			{
-				if (getElement(i) != other[i])
-				{
-					return false;
-				}
-			}
+		// clear List
+		void clear();
 
-			return true;
-		}
-
-		bool operator!=(const List& other) const
-		{
-			return !operator==(other);
-		}
-
-		// access operators
-		const T& getElement(size_t index) const
-		{
-			return mElements[index];
-		}
-		T& getElement(size_t index)
-		{
-			if (index == mElements.size()) mElements.push_back(T());
-			return mElements[index];
-		}
-
-		const T& operator[](size_t index) const { return getElement(index); }
-		T& operator[](size_t index) { return getElement(index); }
-		const T& atBack() const { return getElement(getSize() - 1); }
-		T& atBack() { return getElement(getSize() - 1); }
-
-		// functions
-		void addElement(const T& element) { mElements.push_back(element); }
-		size_t getIndexOf(const T& key) const
-		{
-			for (size_t i = 0; i < getSize(); i++)
-			{
-				if (getElement(i) == key) return i;
-			}
-
-			throw Exception("LIST", "Element does not exist");
-		}
-		bool hasElement(const T& key) const
-		{
-			try
-			{
-				getIndexOf(key);
-			} catch (const Exception&)
-			{
-				return false;
-			}
-
-			return true;
-		}
-
-		// special
-		template <class X>
-		size_t getIndexOf(const X& key) const
-		{
-			for (size_t i = 0; i < getSize(); i++)
-			{
-				if (getElement(i) == key) return i;
-			}
-
-			throw Exception("LIST", "Element does not exist");
-		}
-		template <class X>
-		bool hasElement(const X& key) const
-		{
-			try
-			{
-				getIndexOf(key);
-			} catch (const Exception&)
-			{
-				return false;
-			}
-
-			return true;
-		}
-		size_t getSize() const { return mElements.size(); }
-		void clear() { mElements.clear(); }
+		// element access by key
+		template <class K>
+		bool hasElement(const K& key) const;
+		template <class K>
+		const T& getElement(const K& key) const;
+		template <class K>
+		T& getElement(const K& key);
+		
 	private:
-		std::vector<T> mElements;
+		static const size_t kDefaultSize = 20;
 
-		void initList(T* elements, size_t num)
+		fnd::Vec<T> m_Vec;
+		size_t m_Num;
+	};	
+
+	template<class T>
+	inline List<T>::List() :
+		m_Vec(),
+		m_Num(0)
+	{
+		m_Vec.alloc(kDefaultSize);
+	}
+
+	template<class T>
+	inline List<T>::List(const List<T>& other) :
+		m_Vec(other.m_Vec),
+		m_Size(other.m_Size)
+	{}
+
+	template<class T>
+	inline const List<T>& List<T>::operator=(const List<T>& other)
+	{
+		m_Vec = other.m_Vec;
+		m_Size = other.m_Size;
+	}
+
+	template<class T>
+	inline bool List<T>::operator==(const List<T>& other) const
+	{
+		bool isEqual = true;
+
+		if (m_Num == other.m_Num)
 		{
-			mElements.clear();
-			for (size_t i = 0; i < num; i++)
+			for (size_t i = 0; i < m_Num && isEqual == true; i++)
 			{
-				mElements.push_back(elements[i]);
+				if ((*this)[i] != other[i])
+					isEqual = false;
 			}
 		}
-	};
-}
+		else
+		{
+			isEqual = false;
+		}
 
+		return isEqual;
+	}
+
+	template<class T>
+	inline bool List<T>::operator!=(const List<T>& other) const
+	{
+		return !(*this == other);
+	}
+
+	template<class T>
+	inline void List<T>::addElement(const T & element)
+	{
+		(*this)[m_Num] = element;
+	}
+
+	template<class T>
+	inline const T & List<T>::operator[](size_t index) const
+	{
+		if (index >= m_Num)
+		{
+			throw fnd::Exception("List", "Out of bound read");
+		}
+
+		return m_Vec[index];
+	}
+
+	template<class T>
+	inline T & List<T>::operator[](size_t index)
+	{
+		if (index > m_Num)
+		{
+			throw fnd::Exception("List", "Out of bound read");
+		}
+		else if (index == m_Num)
+		{
+			if ((m_Num * 2) >= m_Vec.size())
+			{
+				m_Vec.alloc((m_Num + 1) * 2);
+			}
+
+			m_Num++;
+		}
+
+		return m_Vec[index];
+	}
+
+	template<class T>
+	inline const T & List<T>::atBack() const
+	{
+		return m_Vec[m_Num - 1];
+	}
+
+	template<class T>
+	inline T & List<T>::atBack()
+	{
+		return m_Vec[m_Num - 1];
+	}
+
+	template<class T>
+	inline size_t List<T>::size() const
+	{
+		return m_Num;
+	}
+
+	template<class T>
+	inline void List<T>::clear()
+	{
+		m_Num = 0;
+		m_Vec.clear();
+	}
+
+	template<class T>
+	template<class K>
+	inline bool List<T>::hasElement(const K & key) const
+	{
+		for (size_t i = 0; i < m_Num; i++)
+		{
+			if (m_List[i] == key)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	template<class T>
+	template<class K>
+	inline const T & List<T>::getElement(const K & key) const
+	{
+		for (size_t i = 0; i < m_Num; i++)
+		{
+			if (m_List[i] == key)
+			{
+				return m_List[i];
+			}
+		}
+
+		throw fnd::Exception("getElement(): element does not exist");
+	}
+
+	template<class T>
+	template<class K>
+	inline T & List<T>::getElement(const K & key)
+	{
+		for (size_t i = 0; i < m_Num; i++)
+		{
+			if (m_List[i] == key)
+			{
+				return m_List[i];
+			}
+		}
+
+		throw fnd::Exception("getElement(): element does not exist");
+	}
+}
