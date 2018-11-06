f6991312f9980330ed67ab76457da460abcac8ac
jakcron <jakcron.dev@gmail.com>
2018-11-06 14:25:30 +0800

    [nstool] Bump version to 1.1

--- programs/nstool/source/version.h
+++ programs/nstool/source/version.h
@@ -2,6 +2,6 @@
 #define APP_NAME	"NSTool"
 #define BIN_NAME	"nstool"
 #define VER_MAJOR	1
-#define VER_MINOR	0
-#define VER_PATCH   5
+#define VER_MINOR	1
+#define VER_PATCH   0
 #define AUTHORS		"jakcron"
