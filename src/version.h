0014f6db9802e00e2afc86fa3aa72ede56e52e29
jakcron <jakcron.dev@gmail.com>
2021-11-16 14:36:16 +0800

    Bump version.

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 #define APP_NAME	"NSTool"
 #define BIN_NAME	"nstool"
 #define VER_MAJOR	1
-#define VER_MINOR	6
-#define VER_PATCH   1
+#define VER_MINOR	7
+#define VER_PATCH	0
 #define AUTHORS		"jakcron"
