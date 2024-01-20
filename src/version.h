b8464efa2752971e3c8492124f7b679420cc72a0
Jack <jack@azulon.net>
2024-01-20 14:05:11 +0800

    Set version to v1.9.0

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
 #define APP_NAME	"NSTool"
 #define BIN_NAME	"nstool"
-#define VER_MAJOR	0
-#define VER_MINOR	0
+#define VER_MAJOR	1
+#define VER_MINOR	9
 #define VER_PATCH   0
 #define AUTHORS		"jakcron"
